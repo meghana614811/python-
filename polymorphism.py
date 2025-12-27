2e9ede7689324c80d686097a4b6ac18d6fba07ab
meghana614811 <meghanakatta61@gmail.com>
2025-12-27 14:30:26 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,23 @@
+class messenger:
+    def send_message(self):
+        print("I am sending a message")
+    def receive_message(self):
+        print("I am receiving a message")
+class instagram(messenger):
+    def send_message(self):
+        print("I am sending a message")
+    def receive_message(self):
+        print("I am receiving a message")
+class Wmessenger(messenger):
+    def send_message(self):
+        print("I am sending a message")
+    def receive_message(messenger):
+        print("I am receiving a message")
+def display(ref):
+    ref.send_message()
+    ref.receiver_message()
+    if ref==W:
+        ref.live_locatio()
+        F=Fmessenger()
+        W=Wmessenger()
+        I=Imessenger()
