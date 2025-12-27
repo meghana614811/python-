550a3d3ae6ec9d64e5e868d3db0d82aaaf033a71
meghana614811 <meghanakatta61@gmail.com>
2025-12-27 14:55:53 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,10 @@
+s=[1,5,7,10,-1]
+largest+=float('-inf')
+smallest+=f;oat('+inf')
+for num in s:
+    if num>largest:
+        largest=num
+if num<smallest:
+    smallest=num
+print(largest,smallest)
+        
